6f4bdf520313c7a7e670230aa4a78b9c423ab714
Balkrishna Pandey <bpandey@redhat.com>
2021-11-05 16:59:28 -0600

    add nameserver
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- platform-none/06-create-cluster.sh
+++ platform-none/06-create-cluster.sh
@@ -1,5 +1,12 @@
+export INFRA_ID=$(jq -r .infraID install-dir/metadata.json)
+
 cd install-dir
 ansible-playbook -i inventory.yaml security-groups.yaml
 ansible-playbook -i inventory.yaml network.yaml
+
+openstack subnet set --dns-nameserver 127.0.0.1 ${INFRA_ID}-nodes
+openstack subnet set --dns-nameserver 8.8.8.8 ${INFRA_ID}-nodes
+openstack subnet set --dns-nameserver 8.8.4.4 ${INFRA_ID}-nodes
+
 ansible-playbook -i inventory.yaml bootstrap.yaml
 ansible-playbook -i inventory.yaml control-plane.yaml
