ebd533f2289e990f16dfc6f249b666780c7b993d
Balkrishna Pandey <bpandey@redhat.com>
2021-11-05 17:12:56 -0600

    custom nameserver pointed to keepalived address
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- platform-none/06-create-cluster.sh
+++ platform-none/06-create-cluster.sh
@@ -4,9 +4,8 @@ cd install-dir
 ansible-playbook -i inventory.yaml security-groups.yaml
 ansible-playbook -i inventory.yaml network.yaml
 
-openstack subnet set --dns-nameserver 127.0.0.1 ${INFRA_ID}-nodes
 openstack subnet set --dns-nameserver 8.8.8.8 ${INFRA_ID}-nodes
-openstack subnet set --dns-nameserver 8.8.4.4 ${INFRA_ID}-nodes
+openstack subnet set --dns-nameserver 10.0.0.5 ${INFRA_ID}-nodes
 
 ansible-playbook -i inventory.yaml bootstrap.yaml
 ansible-playbook -i inventory.yaml control-plane.yaml
