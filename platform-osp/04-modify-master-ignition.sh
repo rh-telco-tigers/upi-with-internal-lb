eb4dcc15cbbc331fb9d998e083e1fe1768d1fdc7
Balkrishna Pandey <bpandey@redhat.com>
2021-11-09 10:49:57 -0700

    master ignition
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- platform-osp/04-modify-master-ignition.sh
+++ platform-osp/04-modify-master-ignition.sh
@@ -7,7 +7,6 @@ ignition = json.load(sys.stdin);
 storage = ignition.get('storage', {});
 files = storage.get('files', []);
 files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
-files.append({'path': '/etc/mdns/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
 storage['files'] = files;
 ignition['storage'] = storage
 json.dump(ignition, sys.stdout)" <install-dir/master.ign > "install-dir/$INFRA_ID-master-$index-ignition.json"
