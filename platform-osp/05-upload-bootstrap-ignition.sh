ddd0fb7f07df1a9fa9e62749cfa27897a02424aa
Balkrishna Pandey <bpandey@redhat.com>
2021-11-09 10:42:33 -0700

    upload ignition config
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- platform-osp/05-upload-bootstrap-ignition.sh
+++ platform-osp/05-upload-bootstrap-ignition.sh
@@ -1,5 +1,5 @@
 export INFRA_ID=$(jq -r .infraID install-dir/metadata.json)
-IGNITION_FILE=$(openstack image create --disk-format=raw --container-format=bare --file install-dir/custom-bootstrap.ign ocp-$INFRA_ID-bootstrap.ign -f json | jq -r .file)
+IGNITION_FILE=$(openstack image create --disk-format=raw --container-format=bare --file install-dir/bootstrap.ign ocp-$INFRA_ID-bootstrap.ign -f json | jq -r .file)
 GLANCE_URL=$(openstack catalog show image -f json | jq -r .endpoints[0].url)
 TOKEN=$(openstack token issue -c id -f value)
 
