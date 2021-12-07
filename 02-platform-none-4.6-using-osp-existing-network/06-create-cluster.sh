4e48ca3b3b8cf4f4b726a4faa36b1aa9f0492cfd
Balkrishna Pandey <bpandey@redhat.com>
2021-12-07 12:54:05 -0700

    create security groups
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- 02-platform-none-4.6-using-osp-existing-network/06-create-cluster.sh
+++ 02-platform-none-4.6-using-osp-existing-network/06-create-cluster.sh
@@ -1,5 +1,6 @@
 export INFRA_ID=$(jq -r .infraID install-dir/metadata.json)
 
 cd install-dir
+ansible-playbook -i inventory.yaml security-groups.yaml
 ansible-playbook -i inventory.yaml bootstrap.yaml
 ansible-playbook -i inventory.yaml control-plane.yaml
