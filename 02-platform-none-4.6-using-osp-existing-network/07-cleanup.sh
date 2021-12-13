61dd1fea368e12451a4de583a8fb6170583d967f
Balkrishna Pandey <bpandey@redhat.com>
2021-12-13 09:49:25 -0700

    cleanup ignition config
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- 02-platform-none-4.6-using-osp-existing-network/07-cleanup.sh
+++ 02-platform-none-4.6-using-osp-existing-network/07-cleanup.sh
@@ -1,4 +1,7 @@
 cd install-dir
+export CONTAINER_NAME="ocp30-ignition"
+export INFRA_ID=$(jq -r .infraID metadata.json)
 ansible-playbook -i inventory.yaml down-bootstrap.yaml
 ansible-playbook -i inventory.yaml down-control-plane.yaml
 ansible-playbook -i inventory.yaml security-groups.yaml
+openstack object delete ${CONTAINER_NAME} ${INFRA_ID}-bootstrap.ign
