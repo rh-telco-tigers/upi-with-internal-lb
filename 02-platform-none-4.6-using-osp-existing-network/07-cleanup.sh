4e12c353ff4660819ca2a50e4a193bb27b69546b
Balkrishna Pandey <bpandey@redhat.com>
2021-12-13 09:55:32 -0700

    cleanup remove security groups
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- 02-platform-none-4.6-using-osp-existing-network/07-cleanup.sh
+++ 02-platform-none-4.6-using-osp-existing-network/07-cleanup.sh
@@ -3,5 +3,5 @@ export CONTAINER_NAME="ocp30-ignition"
 export INFRA_ID=$(jq -r .infraID metadata.json)
 ansible-playbook -i inventory.yaml down-bootstrap.yaml
 ansible-playbook -i inventory.yaml down-control-plane.yaml
-ansible-playbook -i inventory.yaml security-groups.yaml
+ansible-playbook -i inventory.yaml down-security-groups.yaml
 openstack object delete ${CONTAINER_NAME} ${INFRA_ID}-bootstrap.ign
