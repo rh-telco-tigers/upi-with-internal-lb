ca50ce1fa33c901b6af1a719abd0fc4c98730691
Balkrishna Pandey <bpandey@redhat.com>
2021-12-14 15:00:57 -0700

    remove mac address
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- 02-platform-none-4.6-using-osp-existing-network/06-add-ports.sh
+++ 02-platform-none-4.6-using-osp-existing-network/06-add-ports.sh
@@ -1,11 +1,12 @@
-openstack server add port ocp300-lxmsn-master-0 public-ocp300-lxmsn-master-port-0
-openstack server add port ocp300-lxmsn-master-0 management-ocp300-lxmsn-master-port-0 
-openstack server add port ocp300-lxmsn-master-0 outbound-ocp300-lxmsn-master-port-0 
+export INFRA_ID=$(jq -r .infraID metadata.json)
+openstack server add port $INFRA_ID-master-0 public-$INFRA_ID-master-port-0
+openstack server add port $INFRA_ID-master-0 management-$INFRA_ID-master-port-0 
+openstack server add port $INFRA_ID-master-0 outbound-$INFRA_ID-master-port-0 
 
-openstack server add port ocp300-lxmsn-master-1 public-ocp300-lxmsn-master-port-1
-openstack server add port ocp300-lxmsn-master-1 management-ocp300-lxmsn-master-port-1
-openstack server add port ocp300-lxmsn-master-1 outbound-ocp300-lxmsn-master-port-1
+openstack server add port $INFRA_ID-master-1 public-$INFRA_ID-master-port-1
+openstack server add port $INFRA_ID-master-1 management-$INFRA_ID-master-port-1
+openstack server add port $INFRA_ID-master-1 outbound-$INFRA_ID-master-port-1
 
-openstack server add port ocp300-lxmsn-master-2 public-ocp300-lxmsn-master-port-2
-openstack server add port ocp300-lxmsn-master-2 management-ocp300-lxmsn-master-port-2
-openstack server add port ocp300-lxmsn-master-2 outbound-ocp300-lxmsn-master-port-2
+openstack server add port $INFRA_ID-master-2 public-$INFRA_ID-master-port-2
+openstack server add port $INFRA_ID-master-2 management-$INFRA_ID-master-port-2
+openstack server add port $INFRA_ID-master-2 outbound-$INFRA_ID-master-port-2
