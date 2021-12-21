e2d61706f6f29e57907757ec8a196b79c3e34a8c
Balkrishna Pandey <bpandey@redhat.com>
2021-12-20 20:16:02 -0700

    cleanup ports
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- 02-platform-none-4.6-using-osp-existing-network/07-cleanup.sh
+++ 02-platform-none-4.6-using-osp-existing-network/07-cleanup.sh
@@ -4,4 +4,5 @@ export INFRA_ID=$(jq -r .infraID metadata.json)
 ansible-playbook -i inventory.yaml down-bootstrap.yaml
 ansible-playbook -i inventory.yaml down-control-plane.yaml
 ansible-playbook -i inventory.yaml down-security-groups.yaml
+ansible-playbook -i inventory.yaml down-ports.yaml
 openstack object delete ${CONTAINER_NAME} ${INFRA_ID}-bootstrap.ign
