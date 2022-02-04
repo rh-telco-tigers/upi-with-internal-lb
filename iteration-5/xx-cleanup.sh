099c84925394cf99a1a58ac5dd02008d7b8182a0
Balkrishna Pandey <bpandey@redhat.com>
2022-02-04 10:51:42 -0700

    down ports first
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- iteration-5/xx-cleanup.sh
+++ iteration-5/xx-cleanup.sh
@@ -3,6 +3,6 @@ export CONTAINER_NAME=$(yq -r .all.hosts.localhost.swift_container_name inventor
 export INFRA_ID=$(yq -r .all.hosts.localhost.cluster_name inventory.yaml)
 ansible-playbook -i inventory.yaml down-bootstrap.yaml
 ansible-playbook -i inventory.yaml down-control-plane.yaml
-ansible-playbook -i inventory.yaml down-security-groups.yaml
 ansible-playbook -i inventory.yaml down-ports.yaml
+ansible-playbook -i inventory.yaml down-security-groups.yaml
 openstack object delete ${CONTAINER_NAME} ${INFRA_ID}-bootstrap.ign
