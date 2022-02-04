289ea36e377f9e0777f31bf8ef3f3354590ccd51
Balkrishna Pandey <bpandey@redhat.com>
2022-02-03 20:27:04 -0700

    cleanup
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- iteration-5/07-cleanup.sh
+++ iteration-5/07-cleanup.sh
@@ -1,6 +1,6 @@
 cd install-dir
-export CONTAINER_NAME=$(yq -r .all.hosts.localhost.swift_container_name install-dir/inventory.yaml)
-export INFRA_ID=$(yq -r .all.hosts.localhost.cluster_name install-dir/inventory.yaml)
+export CONTAINER_NAME=$(yq -r .all.hosts.localhost.swift_container_name inventory.yaml)
+export INFRA_ID=$(yq -r .all.hosts.localhost.cluster_name inventory.yaml)
 ansible-playbook -i inventory.yaml down-bootstrap.yaml
 ansible-playbook -i inventory.yaml down-control-plane.yaml
 ansible-playbook -i inventory.yaml down-security-groups.yaml
