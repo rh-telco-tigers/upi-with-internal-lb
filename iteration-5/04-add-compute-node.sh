7a8155359d3bdfa0574e40bb633a51c78064b466
Balkrishna Pandey <bpandey@redhat.com>
2022-02-08 11:43:18 -0700

    patch
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- iteration-5/04-add-compute-node.sh
+++ iteration-5/04-add-compute-node.sh
@@ -3,3 +3,4 @@ export INFRA_ID=$(yq -r .all.hosts.localhost.cluster_name install-dir/inventory.
 cd install-dir
 ansible-playbook -i inventory.yaml compute-nodes.yaml 
 oc get csr -o name | xargs oc adm certificate approve
+oc patch -n openshift-ingress-operator ingresscontroller/default --patch '{"spec":{"replicas": 3}}' --type=merge
